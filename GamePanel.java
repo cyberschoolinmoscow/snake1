dfdefe45dbb76eba40fb496c84a1ebf56ca95bd8
cyberschoolinmoscow <cyberschool.moscow@gmail.com>
2022-11-29 19:49:51 +0300

    draw snake and apple

--- GamePanel.java
+++ GamePanel.java
@@ -2,48 +2,116 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
-    int SCREEN_WIDTH=600;
-    int SCREEN_HEIGHT=600;
-    int UNIT_SIZE=25;
-    int GAME_UNITS=(SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-int DELAY=75;
-int[] x=new int [GAME_UNITS];
-int[] y=new int[GAME_UNITS];
-int bodyParts=6;
-int applesEaten;
-int appleX,appleY;
-char direction='R';
-boolean running=false;
-Timer timer;
-Random random;
-    GamePanel(){
-random=new Random();
-this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
-   this.setBackground(Color.BLACK);
-   this.setFocusable(true);
-  // this.addKeyListener(new MyKeyAdapter());
-   startGame();
-    }
-    public void startGame(){
+    int SCREEN_WIDTH = 600;
+    int SCREEN_HEIGHT = 600;
+    int UNIT_SIZE = 25;
+    int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    int DELAY = 75;
+    int[] x = new int[GAME_UNITS];
+    int[] y = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX, appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
+    GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        // this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
-    public void draw(Graphics g){
-for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
-    g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
-    g.drawLine(0,i*UNIT_SIZE,i*UNIT_SIZE,SCREEN_HEIGHT);
-}
+
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
-    public void paintComponent(Graphics g)
-    {
+
+    public void draw(Graphics g) {
+
+        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
+        //newApple();
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+            } else {
+                g.setColor(new Color(45, 180, 0));
+            }
+            g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+
+        }
+    }
+
+    public void newApple() {
+        this.appleX = random.nextInt((int) SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
+        this.appleY = random.nextInt((int) SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
+    }
+
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        switch (direction) {
+            case 'R':
+                x[0] += UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] -= UNIT_SIZE;
+                break;
+            case 'U':
+                y[0] -= UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] += UNIT_SIZE;
+                break;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
+    }
 
+    private void checkCollisions() {
+    }
+
+    private void checkApple() {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 }
