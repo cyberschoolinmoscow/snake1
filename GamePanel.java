564875b6c947d79e9bf53e69f760661d67399f5f
cyberschoolinmoscow <cyberschool.moscow@gmail.com>
2022-11-29 20:01:41 +0300

    snake controls added

--- GamePanel.java
+++ GamePanel.java
@@ -28,7 +28,7 @@ public class GamePanel extends JPanel implements ActionListener {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
-        // this.addKeyListener(new MyKeyAdapter());
+         this.addKeyListener(new MyKeyAdapter());
         startGame();
     }
 
@@ -132,7 +132,28 @@ public class GamePanel extends JPanel implements ActionListener {
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-
+switch (e.getKeyCode()){
+    case KeyEvent.VK_LEFT:
+        if(direction!='R'){
+            direction='L';
+        }
+        break;
+    case KeyEvent.VK_RIGHT:
+        if(direction!='L'){
+            direction='R';
+        }
+        break;
+    case KeyEvent.VK_UP:
+        if(direction!='D'){
+            direction='U';
+        }
+        break;
+    case KeyEvent.VK_DOWN:
+        if(direction!='U'){
+            direction='D';
+        }
+        break;
+}
         }
     }
 }
